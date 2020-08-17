cae932961ef713c32cf7a647969d55c80be84b2d
hjkim <pjt911219@gmail.com>
2020-08-17 11:27:26 +0900

    깃허브 연동

--- src/git_study/HelloJava.java
+++ src/git_study/HelloJava.java
@@ -7,6 +7,7 @@ public class HelloJava {
 		System.out.println("World's end Dancehall");
 		System.out.println("Earth's End Dancehall");
 		System.out.println("Hello Java");
+		System.out.println("github 연동");
 
 	}
 
